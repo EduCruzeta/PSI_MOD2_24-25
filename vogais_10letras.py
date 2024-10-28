2bd0cecdf311b1e537369e87e4ce7540318d5378
Eduardo Cruz <a25053@esenviseu.net>
2024-10-28 16:32:11 +0000

    mod2

--- vogais_10letras.py
+++ vogais_10letras.py
@@ -3,7 +3,11 @@ contar = 0
 for i in range(10):
     letra = input("insira a sua letra: ")
     letra = letra.lower()
-    if letra in "aeiou":
-        contar = contar + 1
+    letras = letra.strip
+    if len(letra) != 1:
+        print("Não é valido insira apenas 1 letra de cada vez")
+    else:
+        if letra in "aeiou":
+            contar = contar + 1
     
 print("inseriu",contar,"vogais")
